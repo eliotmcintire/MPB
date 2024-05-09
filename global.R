3eafee9a31f43df23e8cb922d01b2662c251b5d3
Eliot McIntire <eliotmcintire@gmail.com>
2024-05-09 16:21:36 -0700

    Updated submodule modules/mpbClimateData

--- global.R
+++ global.R
@@ -9,16 +9,18 @@ out <- SpaDES.project::setupProject(
   paths = list(projectPath = "~/GitHub/MPB"),
   modules =
     paste0("achubaty/",
-           c("LandR_MPB_studyArea"
-             ,"mpbClimateData"
-             ,"mpbMassAttacksData"
-             ,"mpbPine"
-             ,"mpbRedTopSpread"), "@HEAD"),
+           c("LandR_MPB_studyArea@development"
+             ,"mpbClimateData@HEAD"
+             ,"mpbMassAttacksData@HEAD"
+             ,"mpbPine@HEAD"
+             ,"mpbRedTopSpread@HEAD")),
   packages = c("PredictiveEcology/reproducible@modsForLargeArchives (HEAD)",
                "RNCan/BioSimClient_R", "googledrive"),
-  options = list(reproducible.useMemoise = TRUE, reproducible.showSimilar = 6),
-  times = list(start = 2010, end = 2030),
-  params = list(.globals = list(.useCache = c(".inputObjects"), .plots = NA)),
+  options = options(reproducible.useMemoise = TRUE, reproducible.showSimilar = 6,
+                    gargle_oauth_email = "eliotmcintire@gmail.com"),
+  times = list(start = 2000, end = 2030),
+  params = list(.globals = list(.useCache = c(".inputObjects", "init"), .plots = NA),
+                mpbClimateData = list(usePrerun = FALSE)),
   Restart = TRUE, useGit = TRUE)
 
 do.call(SpaDES.core::simInitAndSpades, out)

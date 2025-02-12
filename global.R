833e4afc88b05d20ac36cd5712b32381748be8fd
Eliot McIntire <eliotmcintire@gmail.com>
2025-02-11 17:20:22 -0800

    modules & global

--- global.R
+++ global.R
@@ -1,10 +1,10 @@
 repos <- c("https://predictiveecology.r-universe.dev", getOption("repos"))
 source("https://raw.githubusercontent.com/PredictiveEcology/pemisc/development/R/getOrUpdatePkg.R")
 getOrUpdatePkg("Require", minVer = "0.3.1.9098")
-getOrUpdatePkg("pkgload")
-getOrUpdatePkg("SpaDES.project", minVer = c( "0.1.0.9007"))
+# getOrUpdatePkg("pkgload")
+getOrUpdatePkg("SpaDES.project", minVer = c( "0.1.0.9015"))
 # options("SpaDES.project.packages" = NULL)
-pkgload::load_all("~/GitHub/SpaDES.project");
+# pkgload::load_all("~/GitHub/SpaDES.project");
 out <- SpaDES.project::setupProject(
   paths = list(projectPath = "~/GitHub/MPB"),
   modules =
@@ -16,19 +16,23 @@ out <- SpaDES.project::setupProject(
              ,"mpbRedTopSpread@master")),
   packages = c("reproducible (>= 2.1.1)", "terra", "SpaDES.tools (HEAD)", "amc (HEAD)",
                "LandR (HEAD)", "usethis",
+               "PredictiveEcology/reproducible@AI (HEAD)",
                "PredictiveEcology/SpaDES.core@box (HEAD)",
                "BioSIM", "googledrive" ),
-  options = options(reproducible.useMemoise = TRUE, reproducible.showSimilar = 6,
-                    gargle_oauth_email = "eliotmcintire@gmail.com",
-                    gargle_oauth_cache = ".secret",
-                    useRequire = FALSE, reproducible.objSize = FALSE,
+  options = options(reproducible.useMemoise = TRUE,
+                    # reproducible.showSimilar = 6,
+                    # gargle_oauth_email = "eliotmcintire@gmail.com",
+                    gargle_oauth_cache = "~/.secret",
+                    useRequire = FALSE,
+                    reproducible.objSize = FALSE,
                     spades.moduleCodeChecks = FALSE,
                     repos = unique(repos)),
   times = list(start = 2000, end = 2030),
-  params = list(.globals = list(.useCache = c(".inputObjects", "init"), lowMemory = TRUE, .plots = NA),
+  params = list(.globals = list(.useCache = c(".inputObjects", "init"),
+                                lowMemory = TRUE, .plots = NA),
                 mpbClimateData = list(usePrerun = FALSE)),
   Restart = TRUE
-  , useGit = "eliotmcintire"
+ , useGit = "eliotmcintire"
 )
 # pkgload::load_all("~/GitHub/Require"); pkgload::load_all("~/GitHub/reproducible")
-# sim <- do.call(SpaDES.core::simInitAndSpades, out)
+sim <- do.call(SpaDES.core::simInitAndSpades, out)

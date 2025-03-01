12004beb6db12080ef5edff5aaa768864ae8e00b
Eliot McIntire <eliotmcintire@gmail.com>
2025-02-28 16:46:29 -0800

    submodules etc

--- global.R
+++ global.R
@@ -7,6 +7,8 @@ getOrUpdatePkg("SpaDES.project", minVer = c( "0.1.1.9015"))
 # pkgload::load_all("~/GitHub/SpaDES.project")
 # debug(setupPackages)
 out <- SpaDES.project::setupProject(
+  coreForFit = c("localhost",
+             paste0("n", c(18, 161, 174, 179, 202, 207, 181, 202, 207))),
   paths = list(projectPath = "~/GitHub/MPB",
                cachePath = "cache"),
   modules =
@@ -17,15 +19,16 @@ out <- SpaDES.project::setupProject(
              ,"mpbPine@master"
              ,"mpbRedTopSpread@master")),
   packages = c("terra", "PredictiveEcology/SpaDES.tools@pointDistance2", "amc (HEAD)",
-               "LandR (HEAD)", "usethis", "PredictiveEcology/clusters@main",
+               "LandR (HEAD)", "usethis", "PredictiveEcology/clusters@main (HEAD)",
                "PredictiveEcology/reproducible@AI (HEAD)",
                "PredictiveEcology/SpaDES.core@box (HEAD)",
                "BioSIM", "googledrive" ),
   options = options(reproducible.useMemoise = TRUE,
                     reproducible.memoisePersist = TRUE,
                     # reproducible.showSimilar = 6,
-                    # gargle_oauth_email = "eliotmcintire@gmail.com",
+                    gargle_oauth_email = "eliotmcintire@gmail.com",
                     gargle_oauth_cache = "~/.secret",
+                    # gargle_oauth_client = "web",
                     spades.useRequire = FALSE,
                     # SpaDES.project.fast = FALSE,
                     reproducible.inputPaths = "~/data", # This is for prepInputs hardlinks
@@ -37,21 +40,15 @@ out <- SpaDES.project::setupProject(
                     repos = unique(repos)),
   times = list(start = 2000, end = 2030),
   params = list(.globals = list(.useCache = c(".inputObjects", "init"),
-                                lowMemory = TRUE, .plots = c("png"), .runName = "MPB_2"),
+                                lowMemory = TRUE, .plots = c("png"), .runName = "MPB_5"),
                 mpbClimateData = list(usePrerun = FALSE),
                 mpbRedTopSpread = list(type = "DEoptim",
                                        # .runName = "MPB",
-                                       .coresList =
-                                       #  lapply(
-                                       #   # "n161", rep, 30)
-                                       # c("localhost",
-                                       #   paste0("n", c(18, 161, 168, 174, 179, 181))), # 42, 171, and the slower 164 are offline
-                                       #  rep, each = 30)
-                                       # )),
-                                         {df <- data.table::data.table(core = c("localhost",
-                                                                                paste0("n", c(18, 161, 168, 174, 179, 181, 202, 207))),
-                                                                       # paste0("n", c(14, 54, 105))),
-                                                                       ncores = c(80, rep(48, 8))# , rep(16, 3))
+                                       coresListForFitting = # NA
+                                         {df <- data.table::data.table(
+                                           core = coreForFit,
+                                           # paste0("n", c(14, 54, 105))),
+                                           ncores = c(80, rep(48, length.out = length(coreForFit) - 1))# , rep(16, 3))
                                          )
                                          df[, ncoresUse := round(0.75 * ncores)]
                                          df2 <- data.table::data.table(core = rep(df$core, df$ncoresUse))
@@ -70,27 +67,6 @@ out <- SpaDES.project::setupProject(
   # , useGit = "eliotmcintire"
 )
 
-restartOrSimInitAndSpades <- function(out, file = "simPreDispersalFit.qs") {
-  # there are tempdir paths
-  pathsOrig <- out$paths
-  out$paths <- sapply(out$paths, grep, invert = TRUE, value = TRUE, pattern = tempdir(), simplify = TRUE)
-  fn <- function(out) out
-  cached <- attr(reproducible::Cache(fn(out), .functionName = "restartOrSimInitAndSpades"), ".Cache")$newCache %in% FALSE
-  out$paths <- pathsOrig
-  hasSavedToRAMState <- !is.null(SpaDES.core:::savedSimEnv()$.sim)
-  hasSavedToFileState <- file.exists(file)
-  if (!cached || !(hasSavedToFileState || hasSavedToRAMState)) {
-    message("out has changed; rerunning simInitAndSpades")
-    sim <- do.call(SpaDES.core::simInitAndSpades, out)
-  } else {
-    message("out has not changed; trying restartSpades")
-    if (isFALSE(hasSavedToRAMState)) {
-      sim <- SpaDES.core::restartSpades(file)
-    } else  {
-      sim <- SpaDES.core::restartSpades()
-    }
-  }
-}
 
 
 if (FALSE) {
@@ -105,14 +81,15 @@ if (FALSE) {
 
 # pkgload::load_all("~/GitHub/reproducible");
 # pkgload::load_all("~/GitHub/SpaDES.project");
-pkgload::load_all("~/GitHub/clusters");
+# pkgload::load_all("~/GitHub/clusters");
 # pkgload::load_all("~/GitHub/SpaDES.core");
 # pkgload::load_all("~/GitHub/SpaDES.tools");
 # pkgload::load_all("~/GitHub/LandR")
 fn <- "simPreDispersalFit.qs"
 # sim <- SpaDES.core::loadSimList(fn)
-restartOrSimInitAndSpades(out, fn)
-# saveState(file = fn)
+sim <- SpaDES.core::restartOrSimInitAndSpades(out, fn)
+# sim <- restartOrSimInitAndSpades(out, fn) |> Cache()
+saveState(file = fn)
 
 
 # maps <- lapply(mget(grep("studyArea", ls(sim), value = TRUE), envir = envir(sim)), terra::vect)
@@ -121,3 +98,9 @@ restartOrSimInitAndSpades(out, fn)
 # maps <- append(list(area1), maps)
 # names(maps)[1] <- "rasterToMatch_area1"
 # plotSAsLeaflet(sim)
+
+# MPB_3 -- change p_advectionMap to -5 to 5 -- only 7 machines (2 on localhost)
+# MPB_4 -- change p_advectionMap to -25 to 5 -- only 7 machines (2 on localhost)
+# MPB_5 -- fixed the metadata p_advectionMap to 0 to 500 -- 9 machines
+
+# clusters::rmIncompleteDups(out$paths$outputPath)
